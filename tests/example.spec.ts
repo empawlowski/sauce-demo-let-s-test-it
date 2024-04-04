fe630b6c56e21b6498c8f962fbc4b8af46b92e84
Marcin P <marcin.pawlowski@hicron.com>
2024-04-04 15:57:42 +0200

    checkout tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Simple login to page', async ({ page }) => {
+  await page.goto('https://www.example-page.com');
+  await page.getByLabel('User name *').fill('admin');
+  await page.getByLabel('Password *').fill('password');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Log-out')).toBeVisible();
+});
+
+for (let i = 0; i < 100; i++) {
+  test(`Run ${i + 1}`, async ({ page }) => {
+    await page.goto('https://www.example-page.com');
+    await page.getByLabel('User name *').fill('admin');
+    await page.getByLabel('Password *').fill('password');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByText('Log-out')).toBeVisible();
+  });
+}
