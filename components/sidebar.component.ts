66852d5c9806791b9246b684903462085c5daeb7
Marcin P <marcin.pawlowski@hicron.com>
2024-04-23 09:23:01 +0200

    test: add resetApp

--- components/sidebar.component.ts
+++ components/sidebar.component.ts
@@ -7,4 +7,12 @@ export class SideBarComponent {
   linkAbout = this.page.locator('#about_sidebar_link');
   linkLogout = this.page.locator('#logout_sidebar_link');
   linkResetAppState = this.page.locator('#reset_sidebar_link');
+
+  async clickLinkLogout(): Promise<void> {
+    await this.linkLogout.click();
+  }
+
+  async clickLinkResetAppState(): Promise<void> {
+    await this.linkResetAppState.click();
+  }
 }
