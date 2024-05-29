5cf5e5795ba0b01b5df60d783fa1e7b8beb7ee50
Marcin P <marcin.pawlowski@hicron.com>
2024-05-29 13:54:35 +0200

    config: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     baseURL: 'https://www.saucedemo.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
