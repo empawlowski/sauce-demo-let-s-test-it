7e1045d0557bfdceb21b902d079298ee70022150
Marcin P <marcin.pawlowski@hicron.com>
2024-06-03 10:43:47 +0200

    test: add tag

--- tests/performance/load.spec.ts
+++ tests/performance/load.spec.ts
@@ -1,7 +1,8 @@
 import { authData } from '../../.env/.auth/auth.data';
 import { test } from '../../components/fixtures/base';
+import * as report from '../../data/report/playwright.data.json';
 
-test.describe('Load tests', async () => {
+test.describe('Load tests', { tag: [report.tags.load] }, async () => {
   test('Simple login to page', async ({ login, header }) => {
     await login.logIn(authData.standard, authData.password);
     await header.expectLogo();
