055c863ae3249b196080456dd4ccc8c6785c2eb7
Marcin P <marcin.pawlowski@hicron.com>
2024-06-03 10:36:50 +0200

    test: add describe step for load tests

--- tests/performance/load.spec.ts
+++ tests/performance/load.spec.ts
@@ -1,14 +1,16 @@
 import { authData } from '../../.env/.auth/auth.data';
 import { test } from '../../components/fixtures/base';
 
-test('Simple login to page', async ({ login, header }) => {
-  await login.logIn(authData.standard, authData.password);
-  await header.expectLogo();
-});
-
-for (let i = 0; i < 25; i++) {
-  test(`Run ${i + 1}`, async ({ login, header }) => {
+test.describe('Load tests', async () => {
+  test('Simple login to page', async ({ login, header }) => {
     await login.logIn(authData.standard, authData.password);
     await header.expectLogo();
   });
-}
+
+  for (let i = 0; i < 25; i++) {
+    test(`Run ${i + 1}`, async ({ login, header }) => {
+      await login.logIn(authData.standard, authData.password);
+      await header.expectLogo();
+    });
+  }
+});
