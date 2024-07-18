6a04178176f780e283a92d96dd36409f1f0c0de7
Marcin P <marcin.pawlowski@hicron.com>
2024-07-18 14:49:47 +0200

    test: update login test method

--- pages/e2e/login.page.ts
+++ pages/e2e/login.page.ts
@@ -7,6 +7,10 @@ export class LoginPage {
   fieldPassword = this.page.locator('#password');
   bLogin = this.page.locator('#login-button');
 
+  async goto(): Promise<void> {
+    await this.page.goto('/');
+  }
+
   async fillUsername(user: string): Promise<void> {
     await this.fieldUsername.fill(user);
   }
@@ -15,27 +19,31 @@ export class LoginPage {
     await this.fieldPassword.fill(password);
   }
 
+  async clickLogin(): Promise<void> {
+    await this.bLogin.click();
+  }
+
   async logIn(user: string, password: string): Promise<void> {
-    await this.page.goto('/');
+    await this.goto();
     await this.fillUsername(user);
     await this.fillPassword(password);
-    await this.bLogin.click();
+    await this.clickLogin();
   }
 
   async logInWithoutCredentials(): Promise<void> {
-    await this.page.goto('/');
-    await this.bLogin.click();
+    await this.goto();
+    await this.clickLogin();
   }
 
   async logInWithoutUsername(password: string): Promise<void> {
-    await this.page.goto('/');
+    await this.goto();
     await this.fillPassword(password);
-    await this.bLogin.click();
+    await this.clickLogin();
   }
 
   async logInWithoutPassword(user: string): Promise<void> {
-    await this.page.goto('/');
+    await this.goto();
     await this.fillUsername(user);
-    await this.bLogin.click();
+    await this.clickLogin();
   }
 }
