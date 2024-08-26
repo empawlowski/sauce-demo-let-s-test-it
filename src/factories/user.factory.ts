977b99c3aa6dcaf89acc99ca6b2741e0d20ad0c3
Marcin P <marcin.pawlowski@hicron.com>
2024-08-26 12:22:19 +0200

    test: update CheckoutUserModel

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,8 +1,8 @@
-import { CheckoutUser } from '../models/user.model';
+import { CheckoutUserModel } from '../models/user.model';
 import { faker } from '@faker-js/faker';
 
-export function createRandomCheckoutUser(): CheckoutUser {
-  const userCheckoutModel: CheckoutUser = {
+export function createRandomCheckoutUser(): CheckoutUserModel {
+  const userCheckoutModel: CheckoutUserModel = {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
     postalCode: faker.location.zipCode(),
