e9faae157466564ed984ef0820b52267975227ba
Marcin P <marcin.pawlowski@hicron.com>
2024-11-06 14:53:15 +0100

    chore: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,12 @@
-import eslintJsPlugin from '@eslint/js';
-import typeScriptEsLintPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import playwright from 'eslint-plugin-playwright';
-import prettier from 'eslint-plugin-prettier';
+import pluginJs from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: [
-      'package-lock.json',
-      'playwright-report/**',
-      'test-results/**',
-      'test-download/**',
-      'test-upload/**',
-      'src/assets/documents/**',
-      'src/assets/images/**',
-      'src/download/**',
-      'src/upload/**',
-      'src/output/**',
-    ],
-
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parser: typescriptParser,
-      parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: false,
-      },
-    },
-    plugins: {
-      prettier,
-      playwright,
-      '@typescript-eslint': typeScriptEsLintPlugin,
-      typeScriptEsLintPlugin,
-    },
-    rules: {
-      ...eslintJsPlugin.configs.recommended.rules,
-      'no-console': 'warn',
-      'no-unused-vars': 'off',
-      ...typeScriptEsLintPlugin.configs['eslint-recommended'].rules,
-      'typeScriptEsLintPlugin/explicit-function-return-type': 'error',
-      'typeScriptEsLintPlugin/no-unused-vars': 'error',
-      ...playwright.configs.recommended.rules,
-      'playwright/no-nested-step': 'off',
-      ...prettier.configs.recommended.rules,
-      ...eslintConfigPrettier.rules,
-    },
-  },
+  { files: ['**/*.ts'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  { rules: { 'no-console': ['warn', { allow: ['warn', 'error'] }] } },
+  ...tseslint.configs.recommended,
 ];
