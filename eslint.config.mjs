e9a9a53c49ec1f310802083752d3d8c990d8c7f0
Marcin P <marcin.pawlowski@hicron.com>
2024-11-06 15:49:06 +0100

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,21 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    ignores: [
+      'allure-report/**',
+      'allure-results/**',
+      'package-lock.json',
+      'playwright-report/**',
+      'playwright-results/**',
+      'test-results/**',
+      'test-report/**',
+      'src/output/**',
+    ],
+  },
   { files: ['**/*.ts'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  { rules: { 'no-console': ['warn', { allow: ['warn', 'error'] }] } },
+  { rules: { 'no-console': ['off', { allow: ['warn', 'error'] }] } },
   ...tseslint.configs.recommended,
 ];
